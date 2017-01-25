1127fbc73bb9f4b2a9aa8ff623a8a21e56c72144
Jordan Cotter <jrdn.cttr@gmail.com>
2017-01-24 20:16:44 -0600

    Update command.js

--- command.js
+++ command.js
@@ -2,11 +2,11 @@ const log = console.log
 , logE = e => { log ("error: ", e) }
 , logX = x => { log (x) }
 , T = require('data.task')
-,	exec = require('child_process').exec
+, exec = require('child_process').exec
 , command = function (cmd) {
-		return new T (function (reject, resolve) {
-			exec (cmd, (error, stdout, stderr) => {
-				if (error || stderr) reject (error || stderr)
+	return new T (function (reject, resolve) {
+		exec (cmd, (error, stdout, stderr) => {
+			if (error || stderr) reject (error || stderr)
 				else resolve (stdout)
 			})
 		})
